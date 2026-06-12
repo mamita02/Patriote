0ee49957e9684d4f7093e3076a91da4dfe081d8b
mamita02 <mamefatouwade@esp.sn>
2026-06-12 14:39:49 +0000

    modif vite

--- vite.config.ts
+++ vite.config.ts
@@ -7,15 +7,21 @@ export default defineConfig({
       enabled: true,
       prerender: {
         outputPath: "/index.html",
-        crawlLinks: true,
+        crawlLinks: false,
       },
-      // optionnel: prerendre aussi d'autres routes en HTML
     },
     prerender: {
       enabled: true,
-      crawlLinks: true,
-      // ajoute ici les routes publiques connues
-      routes: ["/", "/login"],
+      crawlLinks: false,
+      routes: [
+        "/",
+        "/login",
+        "/dashboard",
+        "/dashboard/academie",
+        "/dashboard/codeveloppement",
+        "/dashboard/parti",
+        "/dashboard/talents",
+      ],
     },
   },
 });
